710108f779dcbc201920cbbf21ca4ae879ef99c6
yujin6789 <115213814+yujin6789@users.noreply.github.com>
2023-03-02 20:16:47 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+/* color palette:
+#082B59 bright blue
+#131D26 navy blue
+#59331D coffee
+#D98555 peach
+#F2F2F2 white
+
+Fonts:
+Headlines - Poppins Bold
+Text - Lato
+
+Links all go to https://uxuicurriculum.wixsite.com/gulproast
+
+*/
